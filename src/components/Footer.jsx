3c3442c5f285801a05b7eed076c583f798bc6891
monov <shohruhulous@gmail.com>
2024-01-14 20:33:17 +0500

    adaptation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,11 @@ const Footer = () => {
       <div className="footer-logo"></div>
       <div className="footer-nav">
         <div className="footer-nav-menu">
-          <a href="">Топ квартиры</a>
-          <a href="">Наш блог</a>
-          <a href="">Почему мы?</a>
-          <a href="">Контакты</a>
+          <a href="">Все квартиры</a>
+          <a href="">По районам</a>
+          <a href="">Продавцу</a>
+          <a href="">О нас</a>
+          <a>Контакты</a>
         </div>
         <p>OOO "VIP UYLAR"</p>
       </div>
