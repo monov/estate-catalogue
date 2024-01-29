976ab246c0293914214ce2dfb95644621a5f0953
monov <shohruhulous@gmail.com>
2024-01-29 14:35:23 +0500

    major design changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,55 @@
 import React from "react";
 import "./Footer.css";
+import { SlPhone, SlLocationPin } from "react-icons/sl";
 
 const Footer = () => {
   return (
     <div className="footer-body">
-      <div className="footer-logo"></div>
-      <div className="footer-nav">
-        <div className="footer-nav-menu">
-          <a href="/allflats">Все квартиры</a>
-          <a href="">По районам</a>
-          <a href="">Продавцу</a>
-          <a href="#whyus">О нас</a>
-          <a href="#contacts">Контакты</a>
+      <div className="ftr-objects logo">
+        <div className="footer-logo"></div>
+      </div>
+      <div className="ftr-objects">
+        <a className="ftr-link" href="#">
+          По районам
+        </a>
+        <a className="ftr-link" href="#">
+          Продавцу
+        </a>
+        <a className="ftr-link" href="#">
+          О нас
+        </a>
+        <a className="ftr-link" href="#">
+          Контакты
+        </a>
+      </div>
+      <div className="ftr-objects rekv">
+        <span>Реквизиты:</span> ООО "VIP UYLAR"
+        <br /> <span>ИНН:</span> 310382762 <br />{" "}
+        <span>Юридический адрес:</span> Toshkent viloyati, Angren sh.,
+        Mustaqillik mahallasi, Bunyodkor ko'chasi, 13-uy, 29-xonadon <br />{" "}
+        <span>р/с:</span> 20208000405639801001 Яшнабадский филиал АКБ
+        "Узпромстройбанк"
+      </div>
+      <div className="ftr-objects cntcts">
+        <div className="ftr-cntcts">
+          <div className="ftr-cntcts wrap">
+            <div>
+              <SlPhone className="ftr-ico"/>
+            </div>
+            <div>
+              +998 (78) 113-08-87 <br />С 09:00 до 18:00
+            </div>
+          </div>
         </div>
-        <p>OOO "VIP UYLAR"</p>
+        <div className="ftr-drs">
+          <div>
+            <SlLocationPin className="ftr-ico"/>
+          </div>
+          <div>
+            Адрес <br />
+            100047, Узбекистан, Ташкент, улица Шахрисабз, д.1Б, 2 этаж
+          </div>
+        </div>
       </div>
     </div>
   );
